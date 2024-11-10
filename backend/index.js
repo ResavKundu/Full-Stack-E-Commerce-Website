d5a38715e477e875e432c6e41a54c938c0dbc134
Resav <resavkundu.student@gmail.com>
2024-11-10 20:21:18 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,17 @@ const connectDB=require('./config/db')
 const router=require('./routes/index')
 
 const app=express()
+// app.use(cors({
+//         origin : process.env.FROENTEND_URL,
+//         credentials:true
+// }))
+const cors = require('cors');
 app.use(cors({
-        origin : process.env.FROENTEND_URL,
-        credentials:true
-}))
+  origin: 'https://full-stack-e-commerce-website-kbch.vercel.app', // Frontend URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(cookieParser())
 app.use(express.json())
 
